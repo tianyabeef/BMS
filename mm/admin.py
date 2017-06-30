fa1e81005bcad8b58338ee83631028f2ab9efdfe
huangyun <410378266@qq.com>
2017-06-30 18:23:44 +0800

    回款大于发票

--- mm/admin.py
+++ mm/admin.py
@@ -239,9 +239,11 @@ class ContractAdmin(ExportActionModelAdmin):
         if amount - income > 0:
             return format_html('<span style="color:{};">{}</span>', 'red', '%s / %s' % (income, amount))
         elif amount - income == 0:
+            if amount == 0 :
+                return '%s/0'%(income)
             return '%s/%s' % (income, obj.fis_date)
         else:
-            return format_html('<span style="color:{};">{}</span>', 'blue','%s / %s' % (income,amount))
+            return format_html('<span style="color:{};">{}</span>', 'blue','%s / %s / %s' % (income,amount,obj.fis_date))
     fis_income.short_description = '首款'
 
     def fin_income(self, obj):
@@ -256,9 +258,11 @@ class ContractAdmin(ExportActionModelAdmin):
         if amount - income > 0:
             return format_html('<span style="color:{};">{}</span>', 'red', '%s / %s' % (income, amount))
         elif amount - income == 0:
+            if amount == 0 :
+                return '%s/0'%(income)
             return '%s/%s' % (income, obj.fin_date)
         else:
-            return format_html('<span style="color:{};">{}</span>', 'blue','%s / %s' % (income,amount))
+            return format_html('<span style="color:{};">{}</span>', 'blue','%s / %s / %s' % (income,amount,obj.fin_date))
     fin_income.short_description = '尾款'
 
     def make_receive(self, request, queryset):
