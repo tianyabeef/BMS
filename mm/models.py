707f7f7b52fb0f9b3ad14cef1e4819dea61bd28d
huangyun <410378266@qq.com>
2018-04-11 08:55:06 +0800

    开票单位

--- mm/models.py
+++ mm/models.py
@@ -49,7 +49,7 @@ class Contract(models.Model):
     contacts = models.CharField('合同联系人',max_length=15, blank=True)
     contact_phone = models.CharField('电话',max_length=30, blank=True)
 
-    contact_note = models.TextField('合同备注',null=True)
+    contact_note = models.TextField('合同备注',blank=True)
 
     class Meta:
         verbose_name = '合同管理'
